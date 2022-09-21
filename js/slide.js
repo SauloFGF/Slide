1d1d67a370ca6824961dc390302410aee43cd217
Saulo Figueiredo <saulo.fgf21@gmail.com>
2022-09-21 14:14:30 -0300

    calculo de distancias

--- js/slide.js
+++ js/slide.js
@@ -1,22 +1,34 @@
 export default class Slide {
     constructor( slide, wrapper) {
-        this.Slide = document.querySelector(slide)
+        this.slide = document.querySelector(slide)
         this.wrapper = document.querySelector(wrapper)
+        this.dist = {finalPosition: 0, startX: 0, movement: 0}
+    }
+
+    moveSlide(distX) {
+        this.dist.movePosition = distX
+        this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
+    }
+
+    updatePosition(clientX) {
+        this.dist.movement = (this.dist.startX - clientX) *1.6;
+        return this.dist.finalPosition - this.dist.movement
     }
 
     onStart(event) {
         event.preventDefault();
-        console.log('mousedown')
+        this.dist.startX = event.clientX
         this.wrapper.addEventListener('mousemove', this.onMove);
     }
 
     onMove(event) {
-        console.log("moveu")
+        const finalPosition = this.updatePosition(event.clientX)
+        this.moveSlide(finalPosition)
     }
 
-    onEnd() {
+    onEnd(event) {
         this.wrapper.removeEventListener('mousemove', this.onMove);
-        console.log('acabou')
+        this.dist.finalPosition = this.dist.movePosition;
     }
 
     addSlideEvents() {
@@ -28,7 +40,6 @@ export default class Slide {
         this.onStart = this.onStart.bind(this);
         this.onMove = this.onMove.bind(this);
         this.onEnd = this.onEnd.bind(this);
-
     }
 
     init() {
